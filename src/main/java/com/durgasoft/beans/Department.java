88dc4fd1cfc6fcbff9dfc350da0ad028ac3e1bb2
mysu <mysasreereddy19@gmail.com>
2025-01-05 20:03:23 +0530

    Department Committed in Pnamespace

---
+++ src/main/java/com/durgasoft/beans/Department.java
@@ -0,0 +1,13 @@
+public class Department
+{
+    private String dname;
+
+    public String getDname()
+    {
+        return dname;
+    }
+    public void setDname(String dname)
+    {
+        this.name=name;
+    }
+}
